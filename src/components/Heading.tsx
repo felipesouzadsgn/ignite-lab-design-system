c6033ecfe2a2a978c694dc59ab533d4c91e6c777
Felipe Souza <felipesouzadsgn@gmail.com>
2022-10-13 16:54:19 -0300

    Adding layout for screen Login

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,17 +6,18 @@ export interface HeadingProps {
   size?: "sm" | "md" | "lg";
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Heading({ size = "md", children, asChild }: HeadingProps) {
+export function Heading({ size = "md", children, asChild, className }: HeadingProps) {
   const Comp = asChild ? Slot : "h2";
   return (
     <Comp
       className={clsx("text-gray-100 font-sans font-bold", {
         "text-lg": size === "sm",
-        "Heading-xl": size === "md",
-        "Heading-2xl": size === "lg",
-      })}
+        "text-xl": size === "md",
+        "text-2xl": size === "lg",
+      }, className)}
     >
       {children}
     </Comp>
